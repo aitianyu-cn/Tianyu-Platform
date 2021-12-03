d383a23f611f84ef979a959c9b3e8a3bea43048d
aitianyu macbook <aitianyu.cn@outlook.com>
2021-12-03 15:22:54 +0800

    New export file and further update
       1. create new files with hxx extension name to use for exporting.
       2. establish dty::text helpers.
    
    branch: dty_native_common
    user: aitianyu.cn@outlook.com

---
+++ dty-native/text/encoding_ascii.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file encoding_ascii.hpp(utils/text)
+ * @author senyun.yao
+ * @brief
+ * @version 0.1
+ * @date 2021-12-03
+ *
+ * @copyright aitianyu.cn Copyright (c) 2021
+ *
+ */
+
+#ifndef __DTY_NATIVE_TEXT_ENCODING_ASCII_H_PLUS_PLUS__
+#define __DTY_NATIVE_TEXT_ENCODING_ASCII_H_PLUS_PLUS__
+
+#include"./encoding.hpp"
+
+namespace dty::text
+{
+    class ASCIIEncoding final : public Encoding
+    {
+
+    };
+}
+
+
+#endif // !__DTY_NATIVE_TEXT_ENCODING_ASCII_H_PLUS_PLUS__
