e29952ef7aa21e6219f509026f82dc17f3ff75ef
victor yao <victor.yao@sap.com>
2021-12-06 17:53:47 +0800

    Start development: DTianyu Test framework
    branch: dty_framework
    user: victor.yao

---
+++ commons/native/test/dtest_core.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file dtest_core.hpp(tianyu-test)
+ * @author victor.yao
+ * @brief
+ * @version 0.1
+ * @date 2021-12-06
+ *
+ * @copyright aitianyu.cn Copyright (c) 2021
+ *
+ */
+
+#ifndef __DTY_FRAMEWORK_TIANYU_TEST_DTEST_CORE_H_PLUS_PLUS__
+#define __DTY_FRAMEWORK_TIANYU_TEST_DTEST_CORE_H_PLUS_PLUS__
+
+#include"./utils.hpp"
+
+ // DTEST_SPEC:     用于定义一个Unit Test，包含topic和Unit Test Name
+ // DTEST_RUN_SPEC: 用于运行指定的Unit Test，需要提供topic和Unit Test Name
+ // DTEST_RUN_ALL:  用于运行所有定义的Unit Test
+ // DTEST_RUN_UNIT: 用于运行指定的一组Unit Test，需要提供topic
+
+ // 需要先定义一个topic，然后基于这个topic定义
+
+#define __AAA(className, body) \
+class AAA_##className  \
+{   \
+    ##body   \
+};  \
+
+
+
+namespace dty::framework::dtest::core
+{
+    abstract class Test
+    {
+
+    };
+
+
+}
+
+#endif // !__DTY_FRAMEWORK_TIANYU_TEST_DTEST_CORE_H_PLUS_PLUS__
---
+++ commons/native/test/utils.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file utils.hpp(tianyu-test)
+ * @author victor.yao
+ * @brief
+ * @version 0.1
+ * @date 2021-12-06
+ *
+ * @copyright aitianyu.cn Copyright (c) 2021
+ *
+ */
+
+#ifndef __DTY_FRAMEWORK_TIANYU_TEST_UTILS_H_PLUS_PLUS__
+#define __DTY_FRAMEWORK_TIANYU_TEST_UTILS_H_PLUS_PLUS__
+
+#include "./utils/utilize.h"
+
+#ifdef __cplusplus
+
+#include "./utils/utilize.hpp"
+
+#endif // !__cplusplus
+
+#endif // !__DTY_FRAMEWORK_TIANYU_TEST_UTILS_H_PLUS_PLUS__
